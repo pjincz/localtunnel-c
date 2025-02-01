43e4f5a929ee744d0243710d0a7db7e9afbe1cf0
Chizhong Jin <pjincz@gmail.com>
2025-02-02 04:34:54 +0800

    cannot find cjson on ubuntu

---
+++ cmake/FindCJSON.cmake
@@ -0,0 +1,31 @@
+find_path(CJSON_INCLUDE_DIR NAMES cjson/cJSON.h
+    PATHS
+        /usr/include
+        /usr/local/include
+        /opt/homebrew/include
+        ENV CPATH
+)
+
+find_library(CJSON_LIBRARY NAMES cjson
+    PATHS
+        /usr/lib
+        /usr/local/lib
+        /opt/homebrew/lib
+        ENV LIBRARY_PATH
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CJSON DEFAULT_MSG CJSON_LIBRARY CJSON_INCLUDE_DIR)
+
+if (CJSON_FOUND)
+    set(CJSON_LIBRARIES ${CJSON_LIBRARY})
+    set(CJSON_INCLUDE_DIRS ${CJSON_INCLUDE_DIR})
+
+    if (NOT TARGET cJSON)
+        add_library(cJSON UNKNOWN IMPORTED)
+        set_target_properties(cJSON PROPERTIES
+            IMPORTED_LOCATION "${CJSON_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${CJSON_INCLUDE_DIR}"
+        )
+    endif()
+endif()
